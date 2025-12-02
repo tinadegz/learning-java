99be1da7b034ba07d220f64ac6d512effcf52e3c
tinaloydegz <mariacristinadegu@yahoo.com>
2025-12-02 15:27:05 +0100

    change directory name and made 1st lesson

---
+++ HelloWorld/Lesson1.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Step 1:Boil 2L of water.");
+        System.out.println("Step 2: Now put your Macaroni Pasta.");
+        System.out.println("Step 3: wait 12 minutes until Pasta is cooked.");
+        System.out.println("Step 4: Drain water.");
+        System.out.println("Step 5:Put your lot of Cheese and milk.");
+        System.out.println("Step6: Mix well.");
+    }
+}
